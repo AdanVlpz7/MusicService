c7bab620a5d3a74408a18af7b160dba7cd8b2e05
adanvlpz <adanvlpz@localhost.localdomain>
2021-02-03 12:29:33 -0600

    loginBase

--- src/app/components/login/login.page.ts
+++ src/app/components/login/login.page.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from "../../services/auth.service"
+import { Router } from "@angular/router"
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  //variables leidas en login.page.html en los ion-input
+  email: string;
+  password :string;
 
-  constructor() { }
+  constructor(private authService : AuthService, public router : Router) { }
 
   ngOnInit() {
   }
 
+  OnSubmitLogin(){
+    //va a llamar a la promesa en auth.service.ts , y si se resuelve, ir a /Home
+    this.authService.login(this.email,this.password).then(res =>{
+      this.router.navigate(['/home'])
+    }).catch(err => alert('Los datos son incorrectos'))
+  }
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {AngularFireAuth} from "@angular/fire/auth"
+import { rejects } from 'assert';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private AFauth : AngularFireAuth) { }
+
+  login(email: string,password : string){
+    return new Promise((resolve,rejected) => {
+      this.AFauth.signInWithEmailAndPassword(email,password).then(user =>{
+        resolve(user);
+      }).catch(err => rejected(err))
+    });
+  }
+}
